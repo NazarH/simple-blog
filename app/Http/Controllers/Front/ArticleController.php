aa78b8c4b055dfacc0d13d13a1e86532249c2045
Nazar Havryliuk <n.havryliuk@ideil.com>
2024-02-22 09:39:28 +0200

    edits3

--- app/Http/Controllers/Front/ArticleController.php
+++ app/Http/Controllers/Front/ArticleController.php
@@ -12,14 +12,8 @@ class ArticleController extends Controller
 {
     public function index(Article $article): View
     {
-        $comments = Comment::query()
-            ->where("article_id", '=', $article->id)
-            ->with('user')
-            ->paginate(1);
-
         return view("pages.article", [
             'article' => $article,
-            'comments' => $comments,
             'rubrics' => Rubric::active()->get()
         ]);
     }
